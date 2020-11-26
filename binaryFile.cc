0f4930329657bb1ec89d1faf4fab35e003bf2002
Zainab Anwar <zeeain14@gmail.com>
2020-11-25 21:35:10 -0600

    binaryFile.cc added

--- binaryFile.cc
+++ binaryFile.cc
@@ -1,3 +1,16 @@
+/*                                                                                                                                                    
+ * Filename:            binaryFile.cc                                                                                                                 
+ * Date:                11/25/2020                                                                                                                    
+ * Author:              Zainab Anwar
+ * Email:               zxa180005@utdallas.edu                                                                                                        
+ * Course CS 3377.002 Fall 2020                                                                                                                      
+ * Version:             1.0                                                                                                                           
+ * Copyright:           2020, All Rights Reserved                                                                                                     
+ *                                                                                                                                                    
+ * Description:                                                                                                                                       
+ * This class includes the BinaryFileHeader class which is used in Main
+ * 
+ */
 #include "header.h"
 //BinaryFileHeader in a separate source file but still being used in Main
 class BinaryFileHeader
